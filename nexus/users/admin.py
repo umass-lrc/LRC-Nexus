3304134be5908a5176ae7e4dae30bb88d6e69c7f
Aadam Lokhandwala <alokhandwala@umass.edu>
2024-01-26 19:43:20 -0500

    Add admin classes for NexusUser, Positions, and PositionGroups

--- nexus/users/admin.py
+++ nexus/users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import (
+    NexusUser,
+    Positions,
+    PositionGroups,
+)
+
+@admin.register(NexusUser)
+class NexusUserAdmin(UserAdmin):
+    pass
+
+@admin.register(Positions)
+class PositionsAdmin(admin.ModelAdmin):
+    list_display = ('semester', 'user', 'faculty', 'hourly_pay')
+    list_filter = ('semester', 'user', 'faculty', 'hourly_pay')
+    search_fields = ('semester', 'user', 'faculty', 'hourly_pay')
+    ordering = ('semester', 'user', 'faculty')
+
+@admin.register(PositionGroups)
+class PositionGroupsAdmin(admin.ModelAdmin):
+    list_display = ('semester', 'name')
+    list_filter = ('semester', 'name')
+    search_fields = ('semester', 'name')
+    ordering = ('semester', 'name')
