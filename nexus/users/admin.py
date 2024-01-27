99661095f0c7c91850f73af09a350c456334b2be
Aadam Lokhandwala <alokhandwala@umass.edu>
2024-01-26 20:08:25 -0500

    Changed the admin model for users

--- nexus/users/admin.py
+++ nexus/users/admin.py
@@ -8,8 +8,11 @@ from .models import (
 )
 
 @admin.register(NexusUser)
-class NexusUserAdmin(UserAdmin):
-    pass
+class NexusUserAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'email')
+    list_filter = ('last_name', 'first_name', 'email')
+    search_fields = ('last_name', 'first_name', 'email')
+    ordering = ('last_name', 'first_name', 'email')
 
 @admin.register(Positions)
 class PositionsAdmin(admin.ModelAdmin):
