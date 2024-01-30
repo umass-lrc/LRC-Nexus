10988b7235aa6c6a72f2cd3e81b9f12638ad2cf7
Aadam Lokhandwala <alokhandwala@umass.edu>
2024-01-30 16:52:25 -0500

    Add load courses functionality

--- nexus/patches/forms.py
+++ nexus/patches/forms.py
@@ -53,3 +53,24 @@ class loadPositionsForm(forms.Form):
                 css_class='text-center',
             ),
         )
+
+class loadCoursesForm(forms.Form):
+    file = forms.FileField()
+
+    def __init__(self, *args, **kwargs):
+        super(loadCoursesForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.attrs = {
+            'hx-post': reverse('load_courses'),
+            'hx-swap': 'multi:#load-courses-message:innerHTML,#load-courses-logs:innerHTML',
+        }
+        self.helper.layout = Layout(
+            Fieldset(
+                '',
+                FloatingField('file'),
+            ),
+            Div(
+                Submit('submit', 'Load Courses', css_class='btn btn-primary'),
+                css_class='text-center',
+            ),
+        )
--- nexus/patches/urls.py
+++ nexus/patches/urls.py
@@ -5,6 +5,8 @@ from .views import (
     load_user_from_line,
     load_positions,
     load_position_from_line,
+    load_courses,
+    load_course_from_line,
 )
 
 urlpatterns = [
@@ -12,4 +14,6 @@ urlpatterns = [
     path('load_users/load_user_from_line/<int:line_number>/', load_user_from_line, name='load_user_from_line'),
     path('load_positions/', load_positions, name='load_positions'),
     path('load_positions/load_position_from_line/<int:line_number>/<int:position>/', load_position_from_line, name='load_position_from_line'),
+    path('load_courses/', load_courses, name='load_courses'),
+    path('load_courses/load_course_from_line/<int:line_number>/', load_course_from_line, name='load_course_from_line'),
 ]
