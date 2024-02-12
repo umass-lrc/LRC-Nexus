2976fa1bb3cd471628929b005104f9354949c728
Aadam Lokhandwala <alokhandwala@umass.edu>
2024-02-12 13:50:05 -0500

    Add API schedule template and views

--- nexus/htmx_apis/urls.py
+++ nexus/htmx_apis/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
+from .views.schedule import (
+    api_schedule_for_all_course,
 
+)
 
 SCHEDULE_URLS = [
+    path("schedule/", api_schedule_for_all_course, name="api_schedule_for_all_course"),
 ]
 
 urlpatterns = (
