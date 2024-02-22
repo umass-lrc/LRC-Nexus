943d270d38d9d9817b4cacec2749949e2ad546cb
Aadam Lokhandwala <alokhandwala@umass.edu>
2024-02-22 11:45:12 -0500

    Add SI and Tutor schedule templates and update schedule view

--- nexus/htmx_apis/views/schedule.py
+++ nexus/htmx_apis/views/schedule.py
@@ -26,13 +26,27 @@ def api_si_schedule_for_all_course(request):
         "dates": [start_date + timedelta(days=i) for i in range(7)],
         "schedule": schedule_for_all_course_for_start(request, start_date, [ShiftKind.SI_SESSION], True),
     }
-    return render(request, "api_schedule.html", context)
+    return render(request, "api_schedule_si.html", context)
 
 @restrict_to_http_methods("GET")
 def api_tutor_schedule_for_all_course(request):
     start_date = timezone.localtime(timezone.now()).date()
+    tutor_schedule = schedule_for_all_course_for_start(request, start_date, [ShiftKind.TUTOR_DROP_IN], True)
+    for course, days in tutor_schedule.items():
+        for i, day in enumerate(days):
+            day_time_coverage = []
+            for shift in day:
+                if len(day_time_coverage) != 0 and shift.start <= day_time_coverage[-1]['end']:
+                    day_time_coverage[-1]['end'] = max(day_time_coverage[-1]['end'], shift.start + shift.duration)
+                else:
+                    day_time_coverage.append({
+                        'start': shift.start,
+                        'end': shift.start + shift.duration,
+                    })
+            tutor_schedule[course][i] = day_time_coverage
     context = {
         "dates": [start_date + timedelta(days=i) for i in range(7)],
-        "schedule": schedule_for_all_course_for_start(request, start_date, [ShiftKind.TUTOR_DROP_IN], True),
+        "schedule": tutor_schedule,
+        "kind": ShiftKind.TUTOR_DROP_IN,
     }
-    return render(request, "api_schedule.html", context)
+    return render(request, "api_schedule_tutor.html", context)
