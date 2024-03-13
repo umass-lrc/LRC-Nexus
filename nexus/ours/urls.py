c34e3b8b88e02d50fb01fbd4dea6f9fe50783de8
Aadam Lokhandwala <alokhandwala@umass.edu>
2024-03-13 07:21:29 -0400

    Add ours app and models

---
+++ nexus/ours/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, re_path
+
+
+FACULTY_URLS = []
+
+OPPORTUNITY_URLS = []
+
+SEARCH_URLS = []
+
+urlpatterns = (
+    FACULTY_URLS +
+    OPPORTUNITY_URLS +
+    SEARCH_URLS
+)
