c8a97712dcda4470d7645488d3400658982a94e0
Ian Mei <ianmmei246@gmail.com>
2025-10-13 02:39:45 -0400

    Revert "changes to tag images and connect properly to elastic search"
    This reverts commit 4ebf18fd138b1e42bdac7716e284ed94f1f00920.

--- nexus/entrypoint.prod.sh
+++ nexus/entrypoint.prod.sh
@@ -1,21 +1,9 @@
 #!/bin/sh
 
-# Set Elasticsearch host
-ES_HOST="http://es-nexus:9200"
-MAX_RETRIES=20
-SLEEP_INTERVAL=3
-
-echo "Waiting for Elasticsearch at $ES_HOST..."
-
-COUNT=0
-while ! curl -s $ES_HOST >/dev/null 2>&1; do
-    # COUNT=$((COUNT+1))
-    # if [ $COUNT -ge $MAX_RETRIES ]; then
-    #     echo "ERROR: Elasticsearch did not start in time!"
-    #     exit 1
-    # fi
-    # echo "Elasticsearch not ready, retrying in $SLEEP_INTERVAL seconds..."
-    sleep $SLEEP_INTERVAL
+# Wait for Elasticsearch to be ready
+echo "Waiting for Elasticsearch..."
+while ! curl -s http://es-nexus:9200 >/dev/null; do
+  sleep 3
 done
 
 echo "Elasticsearch is up! Running migrations, collecting static files, and indexing..."
